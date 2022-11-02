fc184ffb7771bd4d06162d3ae92cadaf52989361
MOHANASUNDARAM K <109417164+mohandotdev@users.noreply.github.com>
2022-11-02 13:29:27 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.get('/',(req,res) =>{
-    res.send("Hello I am expressJS");
-})
-
 app.listen(PORT, ()=>{
     console.log(`Listening on PORT: ${PORT}`);
 })
+
+app.use(express.static("build"));
