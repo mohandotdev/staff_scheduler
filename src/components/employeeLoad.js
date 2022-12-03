a04b3d87f973d62b357e2e34ac4af4a7f0e598f0
MOHANASUNDARAM K <109417164+mohandotdev@users.noreply.github.com>
2022-12-03 11:46:29 +0530

    Revert "code refactoring"
    This reverts commit daf8974b495e27559541c2899e48448f123e38df.

--- src/components/employeeLoad.js
+++
@@ -1,6 +0,0 @@
-export const employeeLoad =
-{
-  morning_up_stairs: { mon: "NA", tue: "NA", wed: "NA", thu: "NA", fri: "NA" },
-  morning_parking_lot: { mon: "NA", tue: "NA", wed: "NA", thu: "NA", fri: "NA" },
-  morning_down_stairs: { mon: "NA", tue: "NA", wed: "NA", thu: "NA", fri: "NA" },
-}
