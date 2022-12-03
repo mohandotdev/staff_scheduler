874bc6b52ebec03b92f5b08c18dd28b0963a4fff
MOHANASUNDARAM K <109417164+mohandotdev@users.noreply.github.com>
2022-12-03 11:56:45 +0530

    code refactoring

---
+++ src/components/employeeLoad.js
@@ -0,0 +1,6 @@
+export const employeeLoad =
+{
+  morning_up_stairs: { mon: "NA", tue: "NA", wed: "NA", thu: "NA", fri: "NA" },
+  morning_parking_lot: { mon: "NA", tue: "NA", wed: "NA", thu: "NA", fri: "NA" },
+  morning_down_stairs: { mon: "NA", tue: "NA", wed: "NA", thu: "NA", fri: "NA" },
+}
